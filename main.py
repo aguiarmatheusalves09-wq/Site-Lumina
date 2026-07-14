fbf7487afbb23931bf27bb3ebd398cc50fe049c4
𝕴𝖈𝖗𝖔𝖜𝖘𝖟 <161983920+Icrowsz@users.noreply.github.com>
2026-07-14 15:49:02 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-from routes import * #Importa todas as coisas do arquivo rotas
-
-#colocar o site no ar
-if __name__ == "__main__": #vai rodar esse if se rodar o codigo nesse arquivo
-    app.run(debug=True) #debug=True atualiza automaticamente as mudanças do site
