e0e46bc88d57fae108f40f480fb02f20896e6037
OBERON <samueljogar.sr@gmail.com>
2026-07-14 15:32:35 -0300

    comits do zaio e do caio
    arrumando erro bobo e incrementando as tabelas de videoaula e artigos. Feito pelo pc do samuel pq os pc do TI nn roda MySQLkkkkkkkj

--- lumina_usuarios.sql
+++ lumina_usuarios.sql
@@ -4,13 +4,38 @@ use lumina;
 create table usuario(email varchar(100) not null Primary key,
 					 senha varchar(100) not null ,
                      nome varchar(100) not null,
-                     data_nasc date not null) ,
+                     data_nasc date not null ,
                      nickName varchar(100)not null);
+                     
 create table atividades(gabarito varchar(200) not null,
 						codigoATIV varchar(100) not null primary key,
-                        emailUSUARIO varchar(100) not null ,
+                        emailUSU varchar(100) not null ,
                         respostaUSU varchar(100) not null,
                         FOREIGN KEY (emailUSU) REFERENCES usuario(email);
-		
+                        
+--codigo feito pelo caio guloso
+
+create table videoaula(codigoVIDEO int auto_increment primary key,
+					titulo varchar(150) not null,
+					descricao text,
+					urlVideo varchar(255) not null,
+					duracao time,
+					dataPublicacao date,
+					materia int not null,
+					foreign key (materia)references materia(codigoMAT)
+);
+
+--codigo feito pelo zaio cabeludo
+
+create table artigo(codigoARTIGO int auto_increment primary key,
+					titulo varchar(150)not null,
+                    resumo text,
+                    conteudo longtext not null,
+                    dataPublicacao date,
+                    materia int not null,
+                    foreign key (materia) references materia(codigoMAT)
+):
+                    
+                    
                         
 	
