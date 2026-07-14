b2eef1f42b83e6dddba411511c43cf344bfc4c19
𝕴𝖈𝖗𝖔𝖜𝖘𝖟 <161983920+Icrowsz@users.noreply.github.com>
2026-07-14 15:48:46 -0300

    Delete lumina.sql

--- lumina.sql
+++
@@ -1,8 +0,0 @@
-create database lumina;
-use lumina;
-
-create table usuario(email varchar(100) not null Primary key,
-					 senha varchar(100) not null ,
-                     nome varchar(100) not null,
-                     data_nasc date not null),
-                     nickName varchar(100)not null);s
